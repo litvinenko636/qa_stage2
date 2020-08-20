a03546f50b684f51adec28befced9bde9ba475a7
litvinenko636 <rabota636R>
2020-08-20 20:01:15 +0300

    commit #3

--- basic_auth.py
+++ basic_auth.py
@@ -1,11 +1,13 @@
 import requests
 from requests.auth import HTTPBasicAuth
+
 from tools.logger import Logger
 
 
 class BasicAuth:
     def __init__(self, url, username, password):
         self.url = url
+        self.username = username
         self.auth = HTTPBasicAuth(username, password)
         self.request = None
 
@@ -14,5 +16,11 @@ class BasicAuth:
         self.request = requests.get(url=self.url, auth=self.auth)
 
     def get_json(self):
-        Logger(__name__).write_info(self.request.json())
-        return self.request.json()
+        data = self.request.json()
+        try:
+            Logger(__name__).write_info(data)
+            assert data['user'] == self.username and data['authenticated'] == True
+            Logger(__name__).write_info('Data assertion is correct!')
+        except AssertionError:
+            Logger(__name__).write_error('Data assertion is incorrect')
+        return data
