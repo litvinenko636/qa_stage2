218a27700d43843d1fc6ca11d8de1b6550f39c1c
litvinenko636 <rabota636R>
2020-08-20 13:23:11 +0300

    commit #1

---
+++ tools/json_reader.py
@@ -0,0 +1,16 @@
+import json
+
+
+class JsonReader:
+    def __init__(self, filename):
+        self.json_file = open(filename, encoding='utf8')
+        self.data = json.load(self.json_file)
+        self.json_file.close()
+
+    def get_username(self):
+        username = self.data["username"]
+        return username
+
+    def get_password(self):
+        password = self.data["password"]
+        return password
---
+++ tools/logger.py
@@ -0,0 +1,22 @@
+import logging
+
+
+class Logger:
+    logging.basicConfig(filename="result.log",
+                        format='%(name)s - %(levelname)s - %(asctime)s - %(message)s',
+                        datefmt='%d-%b-%y %H:%M:%S',
+                        level=logging.INFO,
+                        )
+
+    def __init__(self, filename):
+        self.log = logging
+        self.log = logging.getLogger(filename)
+
+    def write_info(self, var):
+        self.log.info(var)
+
+    def write_warning(self, var):
+        self.log.warning(var)
+
+    def write_error(self, var):
+        self.log.error(var)
