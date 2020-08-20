8c1244d7c3ae511651aa9a89422b2b16ee7cbff7
litvinenko636 <rabota636R>
2020-08-20 20:41:07 +0300

    commit #4

--- framework/browser_actions.py
+++ framework/browser_actions.py
@@ -1,12 +1,18 @@
+from tools.logger import Logger
+
+
 class BrowserActions:
     def __init__(self, driver):
         self.driver = driver
 
     def url_open(self, url):
+        Logger(__name__).write_info(url + " is opened")
         return self.driver.get(url)
 
     def driver_close(self):
+        Logger(__name__).write_info("Driver closed")
         return self.driver.close()
 
     def page_refresh(self):
+        Logger(__name__).write_info("Refresh page")
         return self.driver.refresh()
