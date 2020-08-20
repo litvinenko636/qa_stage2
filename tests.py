18dac59931cae4c7de24ffa8117b6db5d003fc13
litvinenko636 <rabota636R>
2020-08-21 00:37:21 +0300

    commit #3

--- tests.py
+++ tests.py
@@ -1,11 +1,23 @@
 import pytest
 from framework.browser_actions import BrowserActions
+from framework.cookie_tool import CookieTool
 
 
 @pytest.mark.usefixtures('selected_driver', 'data', 'config')
 def test1(selected_driver, data, config):
-
     action = BrowserActions(selected_driver)
     action.url_open(config.get_url())
 
-    # action.driver_close()
+    cookie = CookieTool(selected_driver)
+    cookie.add_some_cookie("example_key_1", data.get_example_key_1())
+    cookie.add_some_cookie("example_key_2", data.get_example_key_2())
+    cookie.add_some_cookie("example_key_3", data.get_example_key_3())
+
+    cookie.delete_cookie("example_key_1")
+
+    cookie.add_some_cookie("example_key_3", data.get_alternate_value())
+    cookie.get_some_cookie("example_key_3")
+
+    cookie.delete_all_cookies()
+
+    action.driver_close()
