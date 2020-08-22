e8dbec4b7121f44cc8ee4349465572e874ef75a6
litvinenko636 <rabota636R>
2020-08-22 16:33:37 +0300

    commit #7

--- main_page.py
+++ main_page.py
@@ -3,25 +3,24 @@ from framework.base_element import BaseElement
 from tools.logger import Logger
 
 
-class MainPageLocators:
-    js_alert_button = (By.XPATH, "//*[@onclick='jsAlert()']")
-    js_confirm_button = (By.XPATH, "//*[@onclick='jsConfirm()']")
-    js_prompt_button = (By.XPATH, "//*[@onclick='jsPrompt()']")
-
-
 class MainPage:
-    def __init__(self, driver):
-        self.driver = driver
-        self.actions = BaseElement(driver)
+    class Button(BaseElement):
+        pass
 
-    def js_alert_button_click(self):
-        Logger(__name__).write_info("js_alert_button is clicked")
-        return self.actions.element_click_by_locator(MainPageLocators.js_alert_button)
 
-    def js_confirm_button_click(self):
-        Logger(__name__).write_info("js_confirm_button is clicked")
-        return self.actions.element_click_by_locator(MainPageLocators.js_confirm_button)
 
-    def js_alert_prompt_click(self):
-        Logger(__name__).write_info("js_prompt_button is clicked")
-        return self.actions.element_click_by_locator(MainPageLocators.js_prompt_button)
+    # def __init__(self, driver):
+    #     self.driver = driver
+    #     self.actions = BaseElement(driver)
+    #
+    # def js_alert_button_click(self):
+    #     Logger(__name__).write_info("js_alert_button is clicked")
+    #     return self.actions.element_click_by_locator(MainPageLocators.js_alert_button)
+    #
+    # def js_confirm_button_click(self):
+    #     Logger(__name__).write_info("js_confirm_button is clicked")
+    #     return self.actions.element_click_by_locator(MainPageLocators.js_confirm_button)
+    #
+    # def js_alert_prompt_click(self):
+    #     Logger(__name__).write_info("js_prompt_button is clicked")
+    #     return self.actions.element_click_by_locator(MainPageLocators.js_prompt_button)
--- tests.py
+++ tests.py
@@ -1,8 +1,15 @@
 import pytest
+from selenium.webdriver.common.by import By
 from framework.browser_actions import BrowserActions
 from framework.modal_dialogs import AlertActions
 from tools.some_tools import random_string
 from main_page import MainPage
+
+
+class MainPageLocators:
+    js_alert_button = (By.XPATH, "//*[@onclick='jsAlert()']")
+    js_confirm_button = (By.XPATH, "//*[@onclick='jsConfirm()']")
+    js_prompt_button = (By.XPATH, "//*[@onclick='jsPrompt()']")
 
 
 @pytest.mark.usefixtures('selected_driver', 'data')
@@ -11,16 +18,15 @@ def test1(selected_driver, data):
     action = BrowserActions(selected_driver)
     action.url_open(data.get_url())
 
-    page = MainPage(selected_driver)
+    MainPage.Button(selected_driver, MainPageLocators.js_alert_button).element_click()
 
-    page.js_alert_button_click()
     alert = AlertActions(selected_driver)
     alert.dialog_accept()
 
-    page.js_confirm_button_click()
+    MainPage.Button(selected_driver, MainPageLocators.js_confirm_button).element_click()
     alert.dialog_accept()
 
-    page.js_alert_prompt_click()
+    MainPage.Button(selected_driver, MainPageLocators.js_prompt_button).element_click()
     alert.dialog_text_input(random_string(10))
     alert.dialog_accept()
 
