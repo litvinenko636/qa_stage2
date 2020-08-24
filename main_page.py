6e37b297c5207b9e685f583d85719a98adf373d6
litvinenko636 <rabota636R>
2020-08-24 12:55:34 +0300

    commit #8

--- main_page.py
+++ main_page.py
@@ -1,26 +1,16 @@
 from selenium.webdriver.common.by import By
-from framework.base_element import BaseElement
-from tools.logger import Logger
+from framework.button import Button
+
+
+class MainPageLocators:
+    js_alert_button = (By.XPATH, "//*[@onclick='jsAlert()']")
+    js_confirm_button = (By.XPATH, "//*[@onclick='jsConfirm()']")
+    js_prompt_button = (By.XPATH, "//*[@onclick='jsPrompt()']")
 
 
 class MainPage:
-    class Button(BaseElement):
-        pass
+    def __init__(self, driver):
+        self.driver = driver
 
-
-
-    # def __init__(self, driver):
-    #     self.driver = driver
-    #     self.actions = BaseElement(driver)
-    #
-    # def js_alert_button_click(self):
-    #     Logger(__name__).write_info("js_alert_button is clicked")
-    #     return self.actions.element_click_by_locator(MainPageLocators.js_alert_button)
-    #
-    # def js_confirm_button_click(self):
-    #     Logger(__name__).write_info("js_confirm_button is clicked")
-    #     return self.actions.element_click_by_locator(MainPageLocators.js_confirm_button)
-    #
-    # def js_alert_prompt_click(self):
-    #     Logger(__name__).write_info("js_prompt_button is clicked")
-    #     return self.actions.element_click_by_locator(MainPageLocators.js_prompt_button)
+    def button_click(self, locator):
+        return Button(self.driver, locator).element_click()
--- tests.py
+++ tests.py
@@ -1,32 +1,25 @@
 import pytest
-from selenium.webdriver.common.by import By
 from framework.browser_actions import BrowserActions
 from framework.modal_dialogs import AlertActions
 from tools.some_tools import random_string
-from main_page import MainPage
-
-
-class MainPageLocators:
-    js_alert_button = (By.XPATH, "//*[@onclick='jsAlert()']")
-    js_confirm_button = (By.XPATH, "//*[@onclick='jsConfirm()']")
-    js_prompt_button = (By.XPATH, "//*[@onclick='jsPrompt()']")
+from main_page import MainPage, MainPageLocators
 
 
 @pytest.mark.usefixtures('selected_driver', 'data')
 def test1(selected_driver, data):
-
     action = BrowserActions(selected_driver)
     action.url_open(data.get_url())
 
-    MainPage.Button(selected_driver, MainPageLocators.js_alert_button).element_click()
+    page = MainPage(selected_driver)
+    page.button_click(MainPageLocators.js_alert_button)
 
     alert = AlertActions(selected_driver)
     alert.dialog_accept()
 
-    MainPage.Button(selected_driver, MainPageLocators.js_confirm_button).element_click()
+    page.button_click(MainPageLocators.js_confirm_button)
     alert.dialog_accept()
 
-    MainPage.Button(selected_driver, MainPageLocators.js_prompt_button).element_click()
+    page.button_click(MainPageLocators.js_prompt_button)
     alert.dialog_text_input(random_string(10))
     alert.dialog_accept()
 
