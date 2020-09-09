b958c0a6596345bc36b893518fde20d53c678574
Max <m.litvinenko@qa-academy.by>
2020-09-09 16:46:17 +0300

    commit #2

--- tests.py
+++ tests.py
@@ -1,13 +1,15 @@
 import pytest
 from framework.browser_tools.browser_actions import BrowserActions
-from main_page import MainPage
-
+from auth_page import AuthPage
 
 @pytest.mark.usefixtures('config', 'data')
 def test1(config, data):
     action = BrowserActions()
     action.url_open(config.get_url())
 
+    auth_page = AuthPage()
+    auth_page.email_input(data.get_email())
+    auth_page.password_input(data.get_password())
+    auth_page.login_button_click()
 
-
-    action.driver_close()
+    # action.driver_close()
